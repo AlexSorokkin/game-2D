c1af568d78240d92d1c4ac8eef54668685afb04e
Alexsorokkin <Alex18041991SOS>
2019-01-30 20:30:55 +0300

    Отредактированы скины. Понизилось время загрузки локации

--- game.py
+++ game.py
@@ -1,5 +1,4 @@
 import pygame
-import os
 import sys
 
 
@@ -19,8 +18,16 @@ class Camera:
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
+        if 'wall' in tile_type:
+            group = wall_group
+        else:
+            group = walk_group
+        super().__init__(group, all_sprites)
+        if tile_type == 'walkgrass':
+            self.image = image_grass
+        elif tile_type == 'wallstone':
+            self.image = image_stone
+        self.image = load_image(level_name, tile_type[4:], 'png')
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
 
@@ -30,11 +37,11 @@ class Player(pygame.sprite.Sprite):
         super().__init__(player_group, all_sprites)
         self.image = player_image
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
+            tile_width * pos_x, tile_height * pos_y)
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
+def load_image(pack, name, png, colorkey=None):
+    fullname = 'data/' + pack + '/' + name + '.' + png
     try:
         image = pygame.image.load(fullname)
         return image
@@ -49,7 +56,7 @@ def terminate():
 
 
 def load_level(filename):
-    filename = "levels/" + filename + '.txt'
+    filename = "level/" + filename + '.txt'
     try:
         with open(filename, 'r') as mapFile:
             level_map = [line.strip() for line in mapFile]
@@ -67,7 +74,7 @@ def start_screen():
                   "Ходить можно только по траве",
                   "Цели пока что нет"]
 
-    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+    fon = pygame.transform.scale(load_image('fon', 'fon', 'jpg'), (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
     text_coord = 50
@@ -85,25 +92,95 @@ def generate_level(level):
     new_player, xx, yy = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
+            Tile('walkgrass', x, y)
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == 's':
+                Tile('wallstone', x, y)
             elif level[y][x] == '@':
-                Tile('empty', x, y)
+                Tile('walklandleft', x, y)
                 xx = x
                 yy = y
-                new_player = Player(x, y)
+            elif level[y][x] == 'S':
+                Tile('wallstones_7', x, y)
+            elif level[y][x] == 'p':
+                Tile('walldecor_1', x, y)
+            elif level[y][x] == 'l':
+                Tile('walllake', x, y)
+            elif level[y][x] == 'b':
+                Tile('walldecor_6', x, y)
+            elif level[y][x] == 'd':
+                Tile('walldecor_5', x, y)
+            elif level[y][x] == 'y':
+                Tile('walklandleft', x, y)
+                Tile('walldecor_3', x, y)
+            elif level[y][x] == 'r':
+                Tile('walldecor_8', x, y)
+            elif level[y][x] == 'z':
+                Tile('walldecor_2', x, y)
+            elif level[y][x] == 'k':
+                Tile('walldecor_7', x, y)
+            elif level[y][x] == 'f':
+                Tile('wallbuilding_3', x, y)
+            elif level[y][x] == 'w':
+                Tile('wallbuilding_2', x, y)
+            elif level[y][x] == 'W':
+                Tile('wallbuilding_1', x, y)
+            elif level[y][x] == 'O':
+                Tile('wallbuilding_5', x, y)
+            elif level[y][x] == 'P':
+                Tile('wallbuilding_4', x, y)
+            elif level[y][x] == '1':
+                Tile('walktree_1', x, y)
+            elif level[y][x] == '2':
+                Tile('walktree_2', x, y)
+            elif level[y][x] == '3':
+                Tile('walktree_3', x, y)
+            elif level[y][x] == '4':
+                Tile('walktree_4', x, y)
+            elif level[y][x] == '5':
+                Tile('walktree_5', x, y)
+            elif level[y][x] == '6':
+                Tile('walktree_6', x, y)
+            elif level[y][x] == '7':
+                Tile('walktree_7', x, y)
+            elif level[y][x] == '8':
+                Tile('walktree_8', x, y)
+            elif level[y][x] == '9':
+                Tile('walktree_9', x, y)
+            elif level[y][x] == 'J':
+                Tile('wallstones_2', x, y)
+            elif level[y][x] == 'K':
+                Tile('walkgreenery_1', x, y)
+            elif level[y][x] == 'L':
+                Tile('walkgreenery_2', x, y)
+            elif level[y][x] == 'D':
+                Tile('walkgreenery_4', x, y)
+            elif level[y][x] == 'E':
+                Tile('walkgreenery_5', x, y)
+            elif level[y][x] == 'Q':
+                Tile('walkgreenery_9', x, y)
+            elif level[y][x] == 'g':
+                Tile('walkgreenery_10', x, y)
+            elif level[y][x] == 't':
+                Tile('walklandleft', x, y)
+            elif level[y][x] == 'T':
+                Tile('walklandup', x, y)
+            elif level[y][x] == 'V':
+                Tile('walklandupper', x, y)
+            elif level[y][x] == 'v':
+                Tile('walklandlefter', x, y)
+            elif level[y][x] == 'F':
+                Tile('wallzdanie', x, y)
+    new_player = Player(xx, yy)
     return new_player, xx, yy
 
 
-tile_images = {
-    'wall': load_image('box.png'),
-    'empty': load_image('grass.png')}
-player_image = load_image('mar.png')
-tile_width = tile_height = 50
+player_image = load_image('character', 'DLE', 'png')
+player_image = pygame.transform.scale(player_image, (100, 100))
+tile_width = tile_height = 100
 pygame.init()
-size = width, height = 400, 300
+size = width, height = 1240, 768
 screen = pygame.display.set_mode(size)
 screen.fill((255, 255, 255))
 FPS = 50
@@ -112,14 +189,17 @@ start_screen()
 startsc = True
 player = None
 all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
+walk_group = pygame.sprite.Group()
+wall_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 camera = Camera()
 level = [[]]
 marx = None
 mary = None
 mario = None
-level_name = "1"
+level_name = "6"
+image_grass = load_image(level_name, 'grass', 'png')
+image_stone = load_image(level_name, 'stone', 'png')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -137,80 +217,16 @@ while True:
                 camera.update(mario)
                 for sprite in all_sprites:
                     camera.apply(sprite)
-        elif event.type == pygame.KEYDOWN and not startsc:
-            if event.key == pygame.K_UP:
-                if level[mary - 1][marx] == '.':
-                    level2 = [[0] * len(level[0]) for _ in range(len(level))]
-                    for y in range(len(level)):
-                        for x in range(len(level[y])):
-                            if x == marx and y == mary - 1:
-                                level2[y][x] = '@'
-                            elif x == marx and y == mary:
-                                level2[y][x] = '.'
-                            elif level[y][x] == '.':
-                                level2[y][x] = '.'
-                            elif level[y][x] == '#':
-                                level2[y][x] = '#'
-                    level = level2
-                    player = generate_level(level)
-                    mario = player[0]
-                    marx = player[1]
-                    mary = player[2]
-            if event.key == pygame.K_LEFT:
-                if level[mary][marx - 1] == '.':
-                    level2 = [[0] * len(level[0]) for _ in range(len(level))]
-                    for y in range(len(level)):
-                        for x in range(len(level[y])):
-                            if x == marx - 1 and y == mary:
-                                level2[y][x] = '@'
-                            elif x == marx and y == mary:
-                                level2[y][x] = '.'
-                            elif level[y][x] == '.':
-                                level2[y][x] = '.'
-                            elif level[y][x] == '#':
-                                level2[y][x] = '#'
-                    level = level2
-                    player = generate_level(level)
-                    mario = player[0]
-                    marx = player[1]
-                    mary = player[2]
-
-            if event.key == pygame.K_DOWN:
-                if level[mary + 1][marx] == '.':
-                    level2 = [[0] * len(level[0]) for _ in range(len(level))]
-                    for y in range(len(level)):
-                        for x in range(len(level[y])):
-                            if x == marx and y == mary + 1:
-                                level2[y][x] = '@'
-                            elif x == marx and y == mary:
-                                level2[y][x] = '.'
-                            elif level[y][x] == '.':
-                                level2[y][x] = '.'
-                            elif level[y][x] == '#':
-                                level2[y][x] = '#'
-                    level = level2
-                    player = generate_level(level)
-                    mario = player[0]
-                    marx = player[1]
-                    mary = player[2]
-            if event.key == pygame.K_RIGHT:
-                if level[mary][marx + 1] == '.':
-                    level2 = [[0] * len(level[0]) for _ in range(len(level))]
-                    for y in range(len(level)):
-                        for x in range(len(level[y])):
-                            if x == marx + 1 and y == mary:
-                                level2[y][x] = '@'
-                            elif x == marx and y == mary:
-                                level2[y][x] = '.'
-                            elif level[y][x] == '.':
-                                level2[y][x] = '.'
-                            elif level[y][x] == '#':
-                                level2[y][x] = '#'
-                    level = level2
-                    player = generate_level(level)
-                    mario = player[0]
-                    marx = player[1]
-                    mary = player[2]
+        elif not startsc:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    mario.rect.y -= 50
+                if event.key == pygame.K_LEFT:
+                    mario.rect.x -= 50
+                if event.key == pygame.K_DOWN:
+                    mario.rect.y += 50
+                if event.key == pygame.K_RIGHT:
+                    mario.rect.x += 50
     if not startsc:
         camera.update(mario)
         for sprite in all_sprites:
